884a1d3d38b2b97e40d2cbf5900c3a41c4f8290b
chintamani_chourase <chintamani_chourase@epam.com>
2025-05-22 01:45:43 +0530

    Reinitialize repository with current state

---
+++ reports-reporting/src/main/java/com/epam/edp/demo/scheduler/WeeklyReportSenderScheduler.java
@@ -0,0 +1,17 @@
+package com.epam.edp.demo.scheduler;
+
+import com.epam.edp.demo.service.ReportSenderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class WeeklyReportSenderScheduler {
+    private final ReportSenderService reportSenderService;
+    @Scheduled(cron = "0 0 10 ? * SAT", zone = "Asia/Kolkata") // Every Saturday at 10:00 AM IST
+    public void scheduledTask() {
+        reportSenderService.sendWeeklyDeltaReport();
+    }
+
+}
